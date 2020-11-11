5414f0e16cfcd234c1a5f6cf834f54ad7e7e7b10
Sjoukje Zaal <sjoukje.zaal@sjoukjezaal.com>
2020-11-11 17:34:32 +0100

    Added Steeltoe Health

--- MusicAPI/Program.cs
+++ MusicAPI/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Steeltoe.Management.Endpoint;
 
 namespace MusicAPI
 {
@@ -21,6 +22,8 @@ namespace MusicAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                //Add Steeltoe Health Endpoint
+                .AddHealthActuator();    
     }
 }
--- MusicApp/Program.cs
+++ MusicApp/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Steeltoe.Management.Endpoint;
 
 namespace MusicApp
 {
@@ -21,6 +22,8 @@ namespace MusicApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                //Add Steeltoe Health Endpoint
+                .AddHealthActuator();  
     }
 }
